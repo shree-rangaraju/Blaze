2936a7d0e7c7f664831ecb3d53a87f01b8260ff6
shree-rangaraju <shree.rangaraju@yahoo.com>
2023-04-10 10:15:59 +0530

    Fixed class name in EventCard

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -133,7 +133,7 @@ export default function EventCard({
                     width="24"
                     height="24"
                     fill="currentColor"
-                    class="bi bi-calendar2-week"
+                    className="bi bi-calendar2-week"
                     viewBox="0 0 16 16"
                   >
                     <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
@@ -152,7 +152,7 @@ export default function EventCard({
                     width="24"
                     height="24"
                     fill="currentColor"
-                    class="bi bi-calendar2-week"
+                    className="bi bi-calendar2-week"
                     viewBox="0 0 16 16"
                   >
                     <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
